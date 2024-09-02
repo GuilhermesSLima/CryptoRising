8f4911f250a9ac5defbfe5fea6ef0cea6f1fba2f
Guilherme de Souza Lima <guilhermedesouzalima2007@gmail.com>
2024-09-02 11:20:47 -0300

    ultimo commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let pAntes = window.scrollY;
 
-window.onscroll = function() {
+window.onscroll = function () {
     let pDepois = window.scrollY;
     let navbar = document.querySelector("header");
 
     if (pAntes > pDepois) {
         navbar.style.top = "0";
     } else {
-        navbar.style.top = "-100px"; 
+        navbar.style.top = "-100px";
     }
     pAntes = pDepois;
 };
