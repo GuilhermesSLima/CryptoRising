050c17394eabaf5b59d73699feb0027a822c34a1
Guilherme de Souza Lima <guilhermedesouzalima2007@gmail.com>
2024-09-22 15:00:29 -0300

    commit

--- paginainicial.js
+++ paginainicial.js
@@ -1,18 +1,4 @@
-let menuIcon = document.getElementById('menuIcon');
-let navMenu = document.getElementById('navMenu');
-
-function toggleMenu() {
-  navMenu.classList.toggle('active');
-  if (navMenu.classList.contains('active')) {
-    navMenu.style.display = 'block';
-  } else {
-    navMenu.style.display = 'none';
-  }
-  if (menuIcon.classList.contains('fa-bars')) {
-    menuIcon.classList.remove('fa-bars');
-    menuIcon.classList.add('fa-times');
-  } else {
-    menuIcon.classList.remove('fa-times');
-    menuIcon.classList.add('fa-bars');
-  }
+function toggleMenu(){
+  const menu = document.getElementById('menu');
+  menu.classList.toggle('show');
 }
