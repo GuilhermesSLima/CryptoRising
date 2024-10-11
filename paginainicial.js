5f55d413982e98d2ec872bb4748b3a41e38f6e9b
Guilherme de Souza Lima <guilhermedesouzalima2007@gmail.com>
2024-10-10 21:28:13 -0300

    commit barra sumindo na mainpage

--- paginainicial.js
+++ paginainicial.js
@@ -2,3 +2,16 @@ window.alternarMenu = function() {
   var menuNavegacao = document.getElementById("menuNavegacao");
   menuNavegacao.classList.toggle("responsive");
 };
+let pAntes = window.scrollY;
+
+window.onscroll = function () {
+    let pDepois = window.scrollY;
+    let navbar = document.querySelector("nav");
+
+    if (pAntes > pDepois) {
+        navbar.style.top = "0";
+    } else {
+        navbar.style.top = "-100px";
+    }
+    pAntes = pDepois;
+};
